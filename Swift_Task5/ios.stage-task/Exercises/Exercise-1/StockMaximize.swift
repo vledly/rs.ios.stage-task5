d9fcb16cfc377b220a03050e7f8302357a84dd0b
Vladislav Meleshko <>
2021-06-13 22:53:04 +0200

    docs: 2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,15 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var result = 0
+        var pricesM = prices
+        
+        for price in prices {
+            pricesM.removeFirst()
+            if let max = pricesM.max(), max > price {
+                result += max - price
+            }
+        }
+        return result
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,63 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        
+        var breketsCounter = 0
+        var submessage = ""
+        var result = ""
+        var number: Int?
+        
+        for item in Array(message).enumerated() {
+            let el = item.element
+            let str = String(el)
+            if let int = Int(str) {
+                if breketsCounter >= 1 {
+                    submessage += str
+                    continue
+                }
+                if number != nil {
+                    number = Int("\(number!)\(int)")!
+                } else {
+                    number = int
+                }
+                continue
+            }
+            
+            switch el {
+            case "[":
+                breketsCounter += 1
+                if breketsCounter > 1 {
+                    submessage += str
+                }
+            case "]":
+                breketsCounter -= 1
+                if breketsCounter == 0 {
+                    let mes = decryptMessage(submessage)
+                    submessage = ""
+                    if var snumber = number {
+                        if snumber == 0 {
+                            continue
+                        }
+                        while snumber > 0 {
+                            snumber -= 1
+                            result += mes
+                        }
+                        number = nil
+                    } else {
+                        result += mes
+                    }
+                } else {
+                    submessage += "]"
+                }
+            default:
+                if breketsCounter == 0 {
+                    result += str
+                } else {
+                    submessage += str
+                }
+            }
+        }
+        
+        return result
     }
 }
